dd324b9cf180420420ac800490a951dcded84dd0
Diksha <dikshakalbhor4@gmail.com>
2023-01-09 10:28:50 -0500

    K6 Scripts

---
+++ specs/periteration.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+import {sleep} from 'k6';
+
+export const options={
+    executor: 'per-vu-iterations',
+    vus: 10,
+    iterations: 200,
+    maxDuration: '30s',
+}
+
+export default function(){
+    http.get('https://test.k6.io');
+    sleep(1);
+}
